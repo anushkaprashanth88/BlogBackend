0f45b458ffa20c3236c1cdad08baa156cb6ce20c
unknown <anushkaprashanth88@gmail.com>
2023-11-17 22:48:04 +0530

    addpost

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from blog.serializer import BlogSerializer
 from blog.models import UserModel
+from blog.models import BlogModel
 
 # Create your views here.
 @csrf_exempt
@@ -24,3 +25,10 @@ def viewUser(request):
         userList = UserModel.objects.all()
         serialize_data = BlogSerializer(userList,many=True)
         return HttpResponse({json.dumps(serialize_data.data)})
+    
+@csrf_exempt
+def addPost(request):
+    if request.method =="POST":
+        post_data = json.loads(request.body)
+        print(json.dumps(post_data))
+        return HttpResponse({json.dumps({"status":"success"})})
