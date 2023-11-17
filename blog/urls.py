04932e876e2a7793c278e1addb6a50805b03635b
unknown <anushkaprashanth88@gmail.com>
2023-11-17 23:23:32 +0530

    viewall

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('add/', views.addUser,name='add'),
     path('view/', views.viewUser,name='view'),
     path('addpost/', views.addPost,name='add'),
+    path('viewall/', views.viewAll,name='viewall'),
 ]
